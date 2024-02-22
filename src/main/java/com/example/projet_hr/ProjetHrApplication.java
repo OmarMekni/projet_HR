9d5126c2ebb8857b284a0ea57664a537d0226a81
omar <omar@gmail.com>
2024-02-22 08:29:28 -0500

    Initial commit

---
+++ src/main/java/com/example/projet_hr/ProjetHrApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_hr/ProjetHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
