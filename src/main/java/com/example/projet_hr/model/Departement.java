659676271ddeca81fed31ff275ee440d668be987
WesLou2023 <137076739+WesLou2023@users.noreply.github.com>
2024-02-26 11:01:56 -0500

    modification de la classe Departement

--- src/main/java/com/example/projet_hr/model/Departement.java
+++ src/main/java/com/example/projet_hr/model/Departement.java
@@ -1,5 +1,61 @@
 package com.example.projet_hr.model;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe Departement qui sera la table départment dans la BD
+ */
+
+@Entity
+@Getter
+@Setter
+@ToString(exclude = {"employees", "jobs"})
+@NoArgsConstructor
+@AllArgsConstructor
 public class Departement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true)
     private String nomDepartement;
+    @OneToMany(mappedBy = "departement", cascade = CascadeType.ALL)
+    private List<Employe> employes;
+    @OneToMany(mappedBy = "departement", cascade = CascadeType.ALL)
+    private List<Emploi> emplois;
+
+    /**
+     * Méthode ajouterEmploye() pour ajouter un employé sur la liste des employés
+     *
+     * @param employe
+     */
+    public void ajouterEmploye(Employe employe) {
+        if (employes == null) {
+            employes = new ArrayList<>();
+        }
+        employes.add(employe);
+    }
+
+    /**
+     * Méthode ajouterEmploi() pour ajouter un emploi sur la liste des jobs
+     *
+     * @param emploi
+     */
+    public void ajouterEmploi(Emploi emploi) {
+        if (emplois == null) {
+            emplois = new ArrayList<>();
+        }
+        emplois.add(emploi);
+    }
+
+
+    @ModelAttribute("departement")
+    public Departement getDepartement() {
+        return new Departement();
+
+    }
 }
