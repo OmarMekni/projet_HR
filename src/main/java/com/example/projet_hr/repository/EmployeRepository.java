ac67e01770e9835c8e035a9fb809bf4415eca5ed
serge paulindrix <6205903@bdeb.qc.ca>
2024-02-26 12:46:57 -0500

    modification de l'interface EmployeRepository

--- src/main/java/com/example/projet_hr/repository/EmployeRepository.java
+++ src/main/java/com/example/projet_hr/repository/EmployeRepository.java
@@ -1,6 +1,7 @@
 package com.example.projet_hr.repository;
 
-import com.example.projet_de_synthese_rh.model.Employe;
+
+import com.example.projet_hr.model.Employe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
